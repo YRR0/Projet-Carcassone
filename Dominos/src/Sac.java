d007f4669179e604f82f24517f8cdd635762d0ad
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2022-11-03 18:14:14 +0100

    Creation de la classe Sac

--- Dominos/src/Sac.java
+++ Dominos/src/Sac.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Sac {
+
+    ArrayList<Tuile> tuiles;
+    static Random rand = new Random();
+
+    public Sac() {
+        tuiles = new ArrayList<Tuile>();
+    }
+
+    public boolean estVide() {
+        return tuiles.size() == 0;
+    }
+
+    public void ajouterTuile(Tuile t) {
+        for(Tuile tuile : tuiles) {
+            if(t.equals(tuiles)) {
+                System.out.println("La tuile est déja présente dans le sac");
+                return;
+            }
+        }
+        tuiles.add(t);
+    }
+
+    public Tuile retirer() {
+        if(!estVide()) {
+            int index = rand.nextInt(tuiles.size());
+            return tuiles.remove(index);
+        }
+        System.out.println("Le sac est vide !");
+        return null;
+    }
 }
