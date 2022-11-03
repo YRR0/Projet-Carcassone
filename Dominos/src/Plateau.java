5759cf15c1b8fd5a77b548f008dbe9a9ae3ecdf3
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2022-11-03 18:13:59 +0100

    Creation de la classe Plateau

--- Dominos/src/Plateau.java
+++ Dominos/src/Plateau.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Plateau {
+
+    ArrayList<ArrayList<Tuile>> cases; //Peut etre pas une bonne idée
+
+    public Plateau() {
+        cases = new ArrayList<ArrayList<Tuile>>();
+    }
+
+    public int nbLin() {
+        return cases.size();
+    }
+
+    public  int nbCol() {
+        return cases.get(0).size();
+    }
+
+    public Tuile getTuile(int i, int j) {
+        return cases.get(i).get(j);
+    }
+
+    public void ajouter(Tuile t, int i, int j) {
+        cases.get(i).set(j, t);
+    }
+
 }
