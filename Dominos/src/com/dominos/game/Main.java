08e5b531877f9cf05151e3611bc74e2a3c28502d
Tsiory <irinararison@gmail.com>
2022-12-26 17:24:08 +0100

    Fix

--- Dominos/src/com/dominos/game/Main.java
+++
@@ -1,9 +0,0 @@
-package com.dominos.game;
-
-import com.dominos.game.controller.GameController;
-
-public class Main {
-    public static void main(String[] args) {
-        new GameController(2).start();
-    }
-}
